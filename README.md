1f10bd6020b651b9c40a8a6ee8b8528606a6df04
Holtz Yan <yan.holtz.data@gmail.com>
2018-07-17 15:49:41 +1000

    Use data to viz template actually

--- README.md
+++ README.md
@@ -4,7 +4,10 @@ Data-Focus
 
 My homepage, where I describe my data analytics activity
 
-URL to come.
+real URL to come.
+
+
+https://holtzy.github.io/Data-Focus/
 
 
 
--- gulpfile.js
+++ gulpfile.js
@@ -58,9 +58,6 @@ gulp.task('css:compile', function() {
     .pipe(sass.sync({
       outputStyle: 'expanded'
     }).on('error', sass.logError))
-    .pipe(header(banner, {
-      pkg: pkg
-    }))
     .pipe(gulp.dest('./css'))
 });
 
@@ -90,9 +87,6 @@ gulp.task('js:minify', function() {
     .pipe(uglify())
     .pipe(rename({
       suffix: '.min'
-    }))
-    .pipe(header(banner, {
-      pkg: pkg
     }))
     .pipe(gulp.dest('./js'))
     .pipe(browserSync.stream());
